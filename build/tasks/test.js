325a347e319a696e60cf2900b2763647f885fdf3
Boubacar Diarra <boubadiarra@gmail.com>
2015-12-18 00:40:37 +0100

    initial import

---
+++ build/tasks/test.js
@@ -0,0 +1,22 @@
+/* global __dirname */
+var gulp = require('gulp');
+var Karma = require('karma').Server;
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  new Karma({
+    configFile: __dirname + '/../../karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
+
+/**
+ * Watch for file changes and re-run tests on each change
+ */
+gulp.task('tdd', function (done) {
+  new Karma({
+    configFile: __dirname + '/../../karma.conf.js'
+  }, done).start();
+});
